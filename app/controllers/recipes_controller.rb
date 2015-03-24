be6243a209c9dbcb241b1e8cc8b84074a9bcff08
Bill Shannon <bill@billshannon.com>
2015-03-24 13:05:04 -0600

    Add like and dislike feature

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,7 @@
 class RecipesController < ApplicationController
 
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.all.sort_by { |likes| likes.thumbs_up_total}.reverse
   end
 
   def show
@@ -29,7 +29,7 @@ class RecipesController < ApplicationController
   end
 
   def update
-@recipe = Recipe.find(params[:id])
+    @recipe = Recipe.find(params[:id])
     if @recipe.update(recipe_params)
       #do something
       flash[:success] = "Your recipe was updated successfully"
@@ -38,6 +38,19 @@ class RecipesController < ApplicationController
       render :edit
     end
   end
+
+  def like
+    @recipe = Recipe.find(params[:id])
+    like = Like.create(like: params[:like], chef: Chef.first, recipe: @recipe)
+    if like.valid?
+      flash[:success] = "Your selection was successful"
+      redirect_to :back
+    else
+      flash[:danger] = "You can only like or dislike a recipe once"
+      redirect_to :back
+    end
+  end
+
 
   private
 
